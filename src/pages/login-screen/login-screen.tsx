217d5321eb8a59e4d89b4f6474df76c5e3e62b5e
Cybranker <cybranker@yandex.ru>
2024-04-03 11:14:18 +0300

    Обновляет компонент LoginScreen

--- src/pages/login-screen/login-screen.tsx
+++ src/pages/login-screen/login-screen.tsx
@@ -1,13 +1,32 @@
+import {useRef, FormEvent} from 'react';
+import {useNavigate} from 'react-router-dom';
 import Meta from '../../components/common/meta';
 import LocationsItem from '../../components/ui/locations-item';
 import {useAppSelector, useAppDispatch} from '../../hooks';
-import {CityNameType} from '../../types';
 import {changeCity} from '../../store/action';
+import {loginAction} from '../../store/api-actions';
+import {AppRoute} from '../../const';
+import {CityNameType} from '../../types';
 
 function LoginScreen(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
   const currentCity = useAppSelector((state) => state.currentCity);
 
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const clickSubmitHandle = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      dispatch(loginAction({
+        login: loginRef.current.value,
+        password: passwordRef.current.value
+      }));
+    }
+  };
 
   const clickChangeCityHandle = (changedCity: CityNameType): void => {
     dispatch(changeCity(changedCity));
@@ -21,10 +40,11 @@ function LoginScreen(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post" onSubmit={clickSubmitHandle}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
                 <input
+                  ref={loginRef}
                   className="login__input form__input"
                   type="email"
                   name="email"
@@ -35,6 +55,7 @@ function LoginScreen(): JSX.Element {
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
                 <input
+                  ref={passwordRef}
                   className="login__input form__input"
                   type="password"
                   name="password"
@@ -42,7 +63,11 @@ function LoginScreen(): JSX.Element {
                   required
                 />
               </div>
-              <button className="login__submit form__submit button" type="submit">
+              <button
+                className="login__submit form__submit button"
+                type="submit"
+                onClick={() => navigate(AppRoute.Root)}
+              >
                 Sign in
               </button>
             </form>
